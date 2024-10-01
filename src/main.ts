58a69e32c09c38ba1df7c7725fcb9a32d3ded3a4
jimyungkoh <kojimyung1206@naver.com>
2024-10-02 04:18:11 +0900

    fix: bootstrap 예외 처리 로직 추가
    - 버그 위험
      - 이전 코드에서는 비동기 함수인 bootstrap에서 발생할 수 있는 예외를 처리하고 있지 않습니다.
    - try-catch 블록을 추가하여 실패했을 때의 오류 처리 코드를 작성하였습니다.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Application is running on: http://localhost:${port}`);
+  } catch (error) {
+    console.error('Error during application startup', error);
+  }
 }
 bootstrap();
