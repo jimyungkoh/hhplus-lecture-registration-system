5d5ce3ee22826d60d600b0652fe51bad04ca7dc4
jimyungkoh <kojimyung1206@naver.com>
2024-10-02 04:09:18 +0900

    chore: eslint 설정 수정 및 설명 주석 추가
    - @typescript-eslint/no-explicit-any은 error로 활성화했습니다.
      - 'any' 타입 사용을 금지합니다.
    - 각 규칙에 대해 왜 비활성화했는지에 대한 주석을 추가하여 비활성화에 대한 이유를 이해할 수 있도록 하였습니다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,9 +17,13 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // 인터페이스 이름 접두사 규칙을 비활성화합니다. 이는 코드 스타일의 유연성을 위해 선택되었습니다.
     '@typescript-eslint/interface-name-prefix': 'off',
+    // 함수 반환 타입을 명시적으로 지정하는 규칙을 비활성화합니다. 이는 타입 추론을 활용하고 코드를 간결하게 유지하기 위함입니다.
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // 모듈 경계에서 타입을 명시적으로 지정하는 규칙을 비활성화합니다. 이는 불필요한 타입 선언을 줄이기 위해 선택되었습니다.
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    // 'any' 타입 사용을 금지합니다. 이는 타입 안정성을 향상시키기 위해 'off'에서 'error'로 변경되었습니다.
+    '@typescript-eslint/no-explicit-any': 'error',
   },
 };
