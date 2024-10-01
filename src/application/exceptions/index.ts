5d767f4aaa6903939865b8c08231913e78efa6b5
jimyungkoh <kojimyung1206@naver.com>
2024-10-02 05:48:10 +0900

    feat: 애플리케이션 - 커스텀 예외
    애플리케이션 예외 처리 클래스 구현
    
    - ApplicationException 추상 클래스 생성
      * ErrorCode를 활용한 일관된 예외 처리 기반 마련
      * status, code, message 관리
    
    - 구체적인 예외 클래스 구현:
      * LectureFullException: 강의 정원 초과 시 발생
      * LectureNotFoundException: 강의를 찾을 수 없을 때 발생
      * UserAlreadyRegisteredException: 사용자가 이미 등록된 경우 발생

---
+++ src/application/exceptions/index.ts
@@ -0,0 +1,4 @@
+export * from './application.exception';
+export * from './lecture-full.exception';
+export * from './lecture-not-found.exception';
+export * from './user-already-registered.exception';
